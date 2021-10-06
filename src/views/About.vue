3278cb98ab67abb53af472e32a167c275ea5564f
Sebastián Castañeda <sebastiansnipez@gmail.com>
2021-10-05 22:01:07 -0500

    added text-center class to about description

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,13 +2,15 @@
   <div
     class="absolute top-2/4 right-2/4 transform translate-x-2/4 -translate-y-2/4	"
   >
-    This website consumes
-    <a
-      class="hover:text-blue-700"
-      href="https://docs.coincap.io/"
-      target="_blank"
-      >Coincap API</a
-    >
+    <p class="text-center">
+      This website consumes
+      <a
+        class="hover:text-blue-700 "
+        href="https://docs.coincap.io/"
+        target="_blank"
+        >Coincap API</a
+      >
+    </p>
   </div>
 </template>
 
